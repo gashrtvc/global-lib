20a4fb1d230577439e8fb9d08516c66f0de6b8c0
gashrtvc <91454223+gashrtvc@users.noreply.github.com>
2023-03-23 15:18:08 +0800

    Update addSidebarLink.groovy

--- vars/addSidebarLink.groovy
+++ vars/addSidebarLink.groovy
@@ -1,5 +1,5 @@
 def call(Map config=[:]) {
   //def action = new hudson.plugins.sidebar_link.LinkAction("http://www.darinpope.com/","testLinkFromDarin","star.gif")
-  def action = new hudson.plugins.sidebar_link(config.url,config.text,config.icon)
+  def action = new hudson.plugins.sidebarr_link.LinkAction(config.url,config.text,config.icon)
   currentBuild.rawBuild.addAction(action)
 }
